2c3ff7dcaf3e95c8ac2dab4f1a3c4275d70a0b68
John Wiseman <jwiseman@morseproject.com>
2014-05-08 15:46:29 -0700

    Added "explode" command line tool for deconstructing navdata packets into one file per navdata option.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/wiseman/turboshrimp"
   :license {:name "MIT License"}
   :dependencies [[ch.qos.logback/logback-classic "1.1.1"]
+                 [com.lemonodor/xio "0.2.2"]
                  [de.kotka/lazymap "3.0.0"]
                  [gloss "0.2.2"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]]
-  :profiles {:dev {:dependencies [[com.lemonodor/xio "0.1.0"]
-                                  [criterium "0.4.3"]
+  :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [expectations "2.0.6"]
                                   [midje "1.6.2"]]
                    :plugins [[lein-cloverage "1.0.2"]
---
+++ src/com/lemondronor/turboshrimp/navdata/explode.clj
@@ -0,0 +1,33 @@
+(ns com.lemondronor.turboshrimp.navdata.explode
+  (:require
+   [clojure.java.io :as io]
+   [com.lemondronor.turboshrimp.navdata :as navdata]
+   [com.lemonodor.xio :as xio])
+  (:gen-class))
+
+
+(defn get-bytes [bb]
+  (.array bb))
+
+
+(defn option-name [type]
+  (if (keyword? type)
+    (name type)
+    (str type)))
+
+
+(def filename-fmt "navdata-%03d-%s.bin")
+
+(defn -main [& args]
+  (let [navdata (xio/binary-slurp (first args))
+        [header options] (navdata/navdata-bytes-seq navdata)]
+    (xio/binary-spit
+     (format filename-fmt 0 "header")
+     (get-bytes header))
+    (loop [idx 1
+           options options]
+      (when (seq options)
+        (let [[type data] (first options)
+              filename (format filename-fmt idx (option-name type))]
+          (xio/binary-spit filename (get-bytes data)))
+        (recur (inc idx) (rest options))))))
