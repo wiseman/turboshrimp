49da49f34cc9512f2946ecce7c9051bbb9984b20
wiseman <jjwiseman@gmail.com>
2014-12-09 22:13:02 -0800

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Clojure API for the Parrot AR.Drone."
   :url "https://github.com/wiseman/turboshrimp"
   :license {:name "MIT License"}
-  :dependencies [[ch.qos.logback/logback-classic "1.1.1"]
+  :dependencies [[ch.qos.logback/logback-classic "1.1.2"]
                  [com.lemonodor/xio "0.2.2"]
                  [de.kotka/lazymap "3.1.1"]
-                 [gloss "0.2.2"]
+                 [gloss "0.2.3"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]]
+                 [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [org.clojars.echo/test.mock "0.1.2"]]
                    :plugins [[lein-cloverage "1.0.2"]]}
