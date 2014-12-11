30ce87ac93bc62128f545d67f1d4731632a51a1a
wiseman <jjwiseman@gmail.com>
2014-12-10 19:52:38 -0800

    Removed dependency on ch.qos.logback/logback-classic.
    We'll use clojure.tools.loggina and let clients use whatever logging
    implementation they want.

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "Clojure API for the Parrot AR.Drone."
   :url "https://github.com/wiseman/turboshrimp"
   :license {:name "MIT License"}
-  :dependencies [[ch.qos.logback/logback-classic "1.1.2"]
-                 [com.lemonodor/xio "0.2.2"]
+  :dependencies [[com.lemonodor/xio "0.2.2"]
                  [de.kotka/lazymap "3.1.1"]
                  [gloss "0.2.3"]
                  [org.clojure/clojure "1.6.0"]
