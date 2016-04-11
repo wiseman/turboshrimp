a7f9743beca87cfd24e27654c1a27f6f2ddba6c6
John Wiseman <jjwiseman@gmail.com>
2016-04-11 12:06:35 -0700

    Version 0.3.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemondronor/turboshrimp "0.3.9-SNAPSHOT"
+(defproject com.lemondronor/turboshrimp "0.3.9"
   :description "Clojure API for the Parrot AR.Drone."
   :url "https://github.com/wiseman/turboshrimp"
   :license {:name "MIT License"
