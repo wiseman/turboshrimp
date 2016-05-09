f3b330286bdb7d700b5bcbcc99244afacffbc980
John Wiseman <jjwiseman@gmail.com>
2016-05-08 22:24:14 -0700

    Bumped version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemondronor/turboshrimp "2.0.3-SNAPSHOT"
+(defproject com.lemondronor/turboshrimp "2.1.0-SNAPSHOT"
   :description "Clojure API for the Parrot AR.Drone 2.0.
 
   By John Wiseman / jjwiseman@gmail.com / http://twitter.com/lemonodor"
